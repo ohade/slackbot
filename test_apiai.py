f51de264b03af3ff0c4dbbe1e9a66b217947d631
Ohad Edelstein <ohadedelstain@gmail.com>
2017-12-03 18:09:07 +0200

    more work on api.ai

--- test_apiai.py
+++ test_apiai.py
@@ -4,12 +4,12 @@ import json
 from config import APIAI_CLIENT_ACCESS_TOKEN
 
 
-def main():
+def simple_test():
     ai = apiai.ApiAI(APIAI_CLIENT_ACCESS_TOKEN)
 
     request = ai.text_request()
 
-    request.lang = 'de'  # optional, default value equal 'en'
+    # request.lang = 'de'  # optional, default value equal 'en'
 
     request.session_id = "<SESSION ID, UNIQUE FOR EACH USER>"
 
@@ -25,5 +25,42 @@ def main():
     print res["result"]["fulfillment"]["speech"]
 
 
+def ifttt_test():
+    ai = apiai.ApiAI(APIAI_CLIENT_ACCESS_TOKEN)
+
+    request = ai.text_request()
+
+    # request.lang = 'de'  # optional, default value equal 'en'
+
+    request.session_id = "<SESSION ID, UNIQUE FOR EACH USER>"
+
+    # request.query = "do that great action after this beautiful action finishes"
+    # request.query = "hello"
+    request.query = "Who are you?"
+    response = request.getresponse()
+
+    # print response.read()
+    # print type(response.read())
+    print type(response)
+    before_json = response.read()
+    res = json.loads(before_json)
+    if "smalltalk" in res["result"]["action"]:
+        print "smalltalk", res["result"]["fulfillment"]["speech"]
+    elif "input.unknown" == res["result"]["action"]:
+        print "input.unknown"
+    else:
+        print res["result"]["metadata"]["intentName"]
+        print res["result"]["fulfillment"]["speech"]
+        print before_json
+
+
 if __name__ == '__main__':
-    main()
+    ifttt_test()
+#
+# after that action finishes do that action
+# do that when that finishes
+# when that finish then do that
+# if that finished then do that
+# if i want to do that then make me do the other thing first
+
+#do that when that finishes
